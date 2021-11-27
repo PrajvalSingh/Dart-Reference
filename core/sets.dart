a851072929abc4e3d2b85cae0ece9ae9df04f227
PrajvalSingh <83118773+PrajvalSingh@users.noreply.github.com>
2021-11-27 09:27:07 +0530

    Added sets.dart

---
+++ core/sets.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Sets are same as Lists but have unique values
+
+  // List
+  List<String> gfg = ['Geeks', 'For', 'Geeks'];
+  print("A list: ${gfg}");
+  // Set
+  Set<String> gfgs = {'Geeks', 'For', 'Geeks'};
+  print("A set: ${gfgs}");
+}
